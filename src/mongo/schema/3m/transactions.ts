246e144456cf9003eac525d874e11907b5219235
MinhLeeBtaskee <leminh.nguyen@btaskee.com>
2023-11-01 17:26:14 +0700

    feat: add schema for transaction

--- src/mongo/schema/3m/transactions.ts
+++ src/mongo/schema/3m/transactions.ts
@@ -1,18 +1,21 @@
 import { mmmDb } from "mongo";
 import { Schema } from "mongoose";
 
-const TransactionsSchema = new Schema({
-  isProduction:Boolean,
-  type:String,
-  createdAt:Date,
-  userId:String,
-  label:{
-    type:String,
-    description:String,
-  value:Number,
-  date:Date
-  }
-});
+const TransactionsSchema = new Schema(
+  {
+    isProduction: Boolean,
+    type: String,
+    createdAt: Date,
+    userId: String,
+    label: {
+      type: String,
+      description: String,
+      value: Number,
+      date: Date,
+    },
+  },
+  { typeKey: "$type" }
+);
 const TransactionsModel = mmmDb.model("transactions", TransactionsSchema);
 
 export default TransactionsModel;
